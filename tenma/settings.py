e4149ba57bc98efa974b0e2a898bf32b7c876671
Harley Hicks <harley@hicks.house>
2017-08-08 17:02:35 -0400

    Database timeout fix.

--- tenma/settings.py
+++ tenma/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+        },
     }
 }
 
